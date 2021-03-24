8f4f42145ba2df8ccfe8ff34d1fe9379cde79808
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-24 01:44:33 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150915202821) do
+
+  create_table "cats", force: :cascade do |t|
+    t.string  "name"
+    t.integer "age"
+    t.string  "breed"
+    t.integer "owner_id"
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
